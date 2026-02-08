4ce3389215175bb2e21513ce08bc51da56a581c1
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-02-08 10:34:14 +0100

    discriminated union (tagged union).

--- 02-using-typescript-with-react-essentials/react-ts-basics/src/components/InfoBox.tsx
+++ 02-using-typescript-with-react-essentials/react-ts-basics/src/components/InfoBox.tsx
@@ -1,20 +1,32 @@
 import type { ReactNode } from 'react'
 
-type InfoBoxProps = {
-  mode: 'hint' | 'warning'
+type HintBoxProps = {
+  mode: 'hint'
   children: ReactNode
 }
 
-export default function InfoBox({ mode, children }: InfoBoxProps) {
-  if (mode === 'hint') {
+type WarningBoxPros = {
+  mode: 'warning'
+  severity: 'low' | 'medium' | 'high'
+  children: ReactNode
+}
+
+type InfoBoxProps = HintBoxProps | WarningBoxPros
+
+export default function InfoBox(props: InfoBoxProps) {
+  const { children, mode } = props
+  if (props.mode === 'hint') {
     return (
       <aside className='infobox infobox-hint'>
         <p>{children}</p>
       </aside>
     )
   }
+
+  const { severity } = props
+
   return (
-    <aside className='infobox infobox-warning warning--medium'>
+    <aside className={`infobox infobox-warning warning--${severity}`}>
       {mode === 'warning' ? <h2>Warning</h2> : null}
       <p>{children}</p>
     </aside>
