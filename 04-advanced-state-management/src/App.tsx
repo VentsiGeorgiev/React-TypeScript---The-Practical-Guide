2f5478d0f94714702af9f6f6fbb7dc0f2d84d272
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-02-08 16:18:36 +0100

    wrap app with timer provider

--- 04-advanced-state-management/src/App.tsx
+++ 04-advanced-state-management/src/App.tsx
@@ -2,16 +2,17 @@ import './App.css'
 import AddTimer from './components/AddTimer'
 import Header from './components/Header'
 import Timers from './components/Timers'
+import TimersContextProvider from './store/timers-context-provider'
 
 function App() {
   return (
-    <>
+    <TimersContextProvider>
       <Header />
       <main>
         <AddTimer />
         <Timers />
       </main>
-    </>
+    </TimersContextProvider>
   )
 }
 
