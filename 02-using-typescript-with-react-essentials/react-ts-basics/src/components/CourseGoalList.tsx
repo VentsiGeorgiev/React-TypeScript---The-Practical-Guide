c9eef540d94b4d4726dd1f490583f77dd2a91c71
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-02-08 10:06:33 +0100

    add info box component

--- 02-using-typescript-with-react-essentials/react-ts-basics/src/components/CourseGoalList.tsx
+++ 02-using-typescript-with-react-essentials/react-ts-basics/src/components/CourseGoalList.tsx
@@ -1,5 +1,7 @@
+import type { ReactNode } from 'react'
 import type { CourseGoalType as CGoal } from '../App'
 import CourseGoal from './CourseGoal'
+import InfoBox from './InfoBox'
 
 type CourseGoalListProps = {
   goals: CGoal[]
@@ -10,18 +12,39 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return (
+      <InfoBox mode='hint'>
+        You have no course goals yet. Start adding some!
+      </InfoBox>
+    )
+  }
+
+  let warningBox: ReactNode
+
+  if (goals.length >= 4) {
+    warningBox = (
+      <InfoBox mode='warning'>
+        You are collecting a lot of goals. Don't put too much on your plate!
+      </InfoBox>
+    )
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal
-            id={goal.id}
-            title={goal.title}
-            description={goal.description}
-            onDelete={onDeleteGoal}
-          />
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal
+              id={goal.id}
+              title={goal.title}
+              description={goal.description}
+              onDelete={onDeleteGoal}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
