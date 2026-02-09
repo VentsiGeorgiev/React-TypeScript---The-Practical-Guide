c9b01cd60205a83786bfbe7f2484482a79de5b84
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-02-09 17:58:04 +0100

    add header and timer functionality

--- 04-advanced-state-management/src/components/Header.tsx
+++ 04-advanced-state-management/src/components/Header.tsx
@@ -1,3 +1,14 @@
+import { useTimersContext } from '../store/timers-context'
+import Button from './UI/Button'
+
 export default function Header() {
-  return <div>Header</div>
+  const { isRunning, startTimers, stopTimers } = useTimersContext()
+  return (
+    <header>
+      <h1>React Timer</h1>
+      <Button el='button' onClick={isRunning ? stopTimers : startTimers}>
+        {isRunning ? 'Stop' : 'Start'} Timers
+      </Button>
+    </header>
+  )
 }
--- 04-advanced-state-management/src/components/Timer.tsx
+++ 04-advanced-state-management/src/components/Timer.tsx
@@ -1,11 +1,52 @@
+import { useEffect, useRef, useState } from 'react'
 import type { Timer as TimerProps } from '../store/timers-type'
 import Container from './UI/Container'
+import { useTimersContext } from '../store/timers-context'
 
 export default function Timer({ name, duration }: TimerProps) {
+  const intervalRef = useRef<number | null>(null)
+  const [remainingTime, setRemainingTime] = useState(duration * 1000)
+  const { isRunning } = useTimersContext()
+
+  useEffect(() => {
+    if (!isRunning) {
+      if (intervalRef.current !== null) {
+        clearInterval(intervalRef.current)
+        intervalRef.current = null
+      }
+      return
+    }
+
+    if (remainingTime === 0) return
+
+    intervalRef.current = setInterval(() => {
+      setRemainingTime((prev) => {
+        const next = Math.max(prev - 50, 0)
+
+        if (next === 0 && intervalRef.current !== null) {
+          clearInterval(intervalRef.current)
+          intervalRef.current = null
+        }
+
+        return next
+      })
+    }, 50)
+
+    return () => {
+      if (intervalRef.current !== null) {
+        clearInterval(intervalRef.current)
+        intervalRef.current = null
+      }
+    }
+  }, [isRunning, remainingTime])
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2)
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>Duration: {duration}</p>
+      <p>{formattedRemainingTime}</p>
     </Container>
   )
 }
