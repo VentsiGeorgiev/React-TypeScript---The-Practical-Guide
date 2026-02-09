805bed399e3c42aa8dce1b41f48046e495a1bb44
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2026-02-09 17:59:08 +0100

    using redux with typescript

---
+++ 06-using-redux-with-typescript/src/components/CartItems.tsx
@@ -0,0 +1,53 @@
+import { addToCart, removeFromCart, type CartItem } from '../store/cart-slice'
+import { useCartDispatch, useCartSelector } from '../store/hooks'
+
+export default function CartItems() {
+  const dispatch = useCartDispatch()
+  const cartItems = useCartSelector((state) => state.cart.items)
+
+  const handleRemoveFromCart = (id: string) => {
+    dispatch(removeFromCart(id))
+  }
+  const handleAddToCart = (item: CartItem) => {
+    dispatch(addToCart(item))
+  }
+
+  const totalPrice = cartItems.reduce(
+    (val, item) => val + item.price * item.quantity,
+    0,
+  )
+  const formattedTotalPrice = totalPrice.toFixed(2)
+
+  return (
+    <div id='cart'>
+      {cartItems.length <= 0 ? (
+        <p>No items in cart!</p>
+      ) : (
+        <ul id='cart-items'>
+          {cartItems.map((item) => {
+            const formattedPrice = `$${item.price.toFixed(2)}`
+
+            return (
+              <li key={item.id}>
+                <div>
+                  <span>{item.title}</span>
+                  <span> ({formattedPrice})</span>
+                </div>
+                <div className='cart-item-actions'>
+                  <button onClick={() => handleRemoveFromCart(item.id)}>
+                    -
+                  </button>
+                  <span>{item.quantity}</span>
+                  <button onClick={() => handleAddToCart(item)}>+</button>
+                </div>
+              </li>
+            )
+          })}
+          <p id='cart-total-price'>
+            Cart Total: <strong>{formattedTotalPrice}</strong>
+          </p>
+        </ul>
+      )}
+    </div>
+  )
+}
